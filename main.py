b30feaa9edc93e7c72ce54fb85f9e67317888197
usernameclaimer <adenolmstead61@gmail.com>
2024-11-23 19:46:37 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+# URL of the Discord invite link
+discord_invite_link = "https://discord.gg/h7YCxhE2d"
+
+# Open the Discord invite link in the default web browser
+webbrowser.open(discord_invite_link)
